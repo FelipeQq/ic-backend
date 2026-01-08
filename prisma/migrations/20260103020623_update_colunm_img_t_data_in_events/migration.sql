70f58b071da7fad26178b36415db610891053e3e
jjuniorssilva <juniorrsilvaa.dj@gmail.com>
2026-01-07 21:02:11 -0300

    feat: remove deprecated columns and tables; update roles and payments structure in the database schema

--- prisma/migrations/20260103020623_update_colunm_img_t_data_in_events/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `img` on the `events` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "events" DROP COLUMN "img",
-ADD COLUMN     "data" JSONB;
