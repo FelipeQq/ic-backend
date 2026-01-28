e9084396c56a26c0c056ecf3d1bad97a8c73bf9a
jjuniorssilva <juniorrsilvaa.dj@gmail.com>
2026-01-28 17:22:16 -0300

    fix: remove unnecessary whitespace in PrismaService class

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -149,7 +149,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
       return result;
     });
   }
-
+  //-
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close();
